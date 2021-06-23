1df5f81b6c97ad12ed6d30401db7acffbf42d2ec
Mileni Evelin <1kn.evie@gmail.com>
2021-06-23 16:05:05 -0300

    adc btn excluir cliente

--- app/src/main/java/com/app/infinitesystem/controlador/ListaClienteActivity.java
+++ app/src/main/java/com/app/infinitesystem/controlador/ListaClienteActivity.java
@@ -1,12 +1,17 @@
 package com.app.infinitesystem.controlador;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
@@ -37,12 +42,16 @@ public class ListaClienteActivity extends AppCompatActivity {
         clientesFiltrados.addAll(clientes);
         ArrayAdapter<Cliente> adaptador = new ArrayAdapter<>(getApplication(), android.R.layout.simple_list_item_1, clientesFiltrados);
         listView.setAdapter(adaptador);
+
+        registerForContextMenu(listView);//para menu contexto
+
     }
 
     public boolean onCreateOptionsMenu (Menu menu) {
         MenuInflater i = getMenuInflater();
         i.inflate(R.menu.menu_cli, menu);
 
+        //Procurando pela String no Botão
         SearchView searchView = (SearchView) menu.findItem(R.id.app_bar_search_cli).getActionView();
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
@@ -70,6 +79,34 @@ public class ListaClienteActivity extends AppCompatActivity {
         }
 
         listView.invalidateViews();
+    }
+
+    //Menu Contexto Cliente
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
+        super.onCreateContextMenu(menu,v,menuInfo);
+        MenuInflater i = getMenuInflater();
+        i.inflate(R.menu.menu_cont_cli, menu);
+    }
+
+    //Excluindo Cliente
+    public void excluirCliente(MenuItem item){
+        AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        final Cliente clienteExcluir = clientesFiltrados.get(menuInfo.position);
+
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("Atenção")
+                .setMessage("Realmente deseja excluir?")
+                .setNegativeButton("Não",null)
+                .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        clientesFiltrados.remove(clienteExcluir);
+                        clientes.remove(clienteExcluir);
+                        dao.excluirCli(clienteExcluir);
+                        listView.invalidateViews();
+                    }
+                }).create();
+        dialog.show();
     }
 
 
