fcef09440445fc154579072d02d5b46ef782cd5d
Mileni Evelin <1kn.evie@gmail.com>
2021-06-24 19:38:25 -0300

    adc menus agendamento e activity listaAgenda

---
+++ app/src/main/java/com/app/infinitesystem/controlador/ListaAgendaActivity.java
@@ -0,0 +1,16 @@
+package com.app.infinitesystem.controlador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.infinitesystem.R;
+
+public class ListaAgendaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_agenda);
+    }
+}
